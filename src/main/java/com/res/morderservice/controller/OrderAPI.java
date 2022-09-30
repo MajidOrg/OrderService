d5909bdaeb814070c9af121638ca5a7044b04195
amitdatta <the.amisoft@gmail.com>
2022-09-30 11:17:19 +0530

    Swagger dependency added

---
+++ src/main/java/com/res/morderservice/controller/OrderAPI.java
@@ -0,0 +1,25 @@
+package com.res.morderservice.controller;
+
+import com.res.morderservice.dto.RestaurantOrder;
+import com.res.morderservice.response.OrderResponse;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Configuration
+@Tag(name = "The Restaurant application ")
+@OpenAPIDefinition
+public interface OrderAPI {
+
+    @Operation(summary = "This API takes a new order from the customer")
+    @ApiResponses(value = {@ApiResponse(responseCode = "500", description = "Internal server error"),
+            @ApiResponse(responseCode = "200", description = "Ok")})
+    @PostMapping("/createOrder")
+    ResponseEntity<OrderResponse> createOrder(@RequestBody RestaurantOrder restaurantOrder);
+}
--- src/main/java/com/res/morderservice/controller/OrderController.java
+++ src/main/java/com/res/morderservice/controller/OrderController.java
@@ -4,38 +4,24 @@ package com.res.morderservice.controller;
 import com.res.morderservice.dto.RestaurantOrder;
 import com.res.morderservice.response.OrderResponse;
 import com.res.morderservice.service.OrderService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Tag(name = "The Restaurant application ")
-public class OrderController {
-
+public class OrderController implements OrderAPI {
     private OrderService orderService;
-
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
     }
 
-    @PostMapping("/createOrder")
-    @Operation(summary = "This API takes a new order from the customer")
-    @ApiResponses(value = {@ApiResponse(responseCode = "500", description = "Internal server error"),
-            @ApiResponse(responseCode = "200", description = "Ok")})
-    ResponseEntity<OrderResponse> createOrder(@RequestBody RestaurantOrder restaurantOrder){
+    public ResponseEntity<OrderResponse> createOrder(@RequestBody RestaurantOrder restaurantOrder) {
 
-        if(null == restaurantOrder ||
-                null == restaurantOrder.getDishOrdered()){
-
+        if (null == restaurantOrder ||
+                null == restaurantOrder.getDishOrdered()) {
             return ResponseEntity.badRequest().build();
         }
-
         var orderResponse = orderService.createOrder(restaurantOrder);
         return ResponseEntity.status(HttpStatus.OK).body(orderResponse);
     }
