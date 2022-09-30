54b46084e8a24b360d51d6cff850ff98b6471825
amitdatta <the.amisoft@gmail.com>
2022-09-30 10:59:13 +0530

    Swagger dependency added

--- src/main/java/com/res/morderservice/dto/RestaurantOrder.java
+++ src/main/java/com/res/morderservice/dto/RestaurantOrder.java
@@ -1,6 +1,7 @@
 package com.res.morderservice.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +11,8 @@ import lombok.Setter;
 @Builder
 public class RestaurantOrder {
 
+    @Schema(description = "Restaurant customer  name")
     private String customerName;
+    @Schema(description = "Menu that customer prefer to eat")
     private String dishOrdered;
 }
--- src/main/java/com/res/morderservice/response/OrderResponse.java
+++ src/main/java/com/res/morderservice/response/OrderResponse.java
@@ -1,6 +1,7 @@
 package com.res.morderservice.response;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +11,8 @@ import lombok.Setter;
 @Builder
 public class OrderResponse {
 
+    @Schema(description = "The order reference number")
     private Long orderId;
+    @Schema(description = "The details of the order")
     private String orderDetails;
 }
