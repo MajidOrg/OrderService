de09ca71d251f7de728f7d42449baed532aa66df
amitdatta <the.amisoft@gmail.com>
2022-10-03 11:28:00 +0530

    H2 database and JPA

--- src/main/java/com/res/morderservice/service/OrderService.java
+++ src/main/java/com/res/morderservice/service/OrderService.java
@@ -1,21 +1,31 @@
 package com.res.morderservice.service;
 
 import com.res.morderservice.dto.RestaurantOrder;
+import com.res.morderservice.entity.Order;
+import com.res.morderservice.repository.OrderRepo;
 import com.res.morderservice.response.OrderResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class OrderService {
 
+    OrderRepo orderRepo;
+    public OrderService(OrderRepo orderRepo) {
+        this.orderRepo = orderRepo;
+    }
 
     public OrderResponse createOrder(RestaurantOrder restaurantOrder) {
 
-        //TODO -: Need to add business logic to save order
+        var order = new Order();
+        BeanUtils.copyProperties(restaurantOrder,order);
+        var savedOrder = orderRepo.save(order);
+
         return OrderResponse.builder()
-                .orderDetails("Mango")
-                .orderId(123L)
+                .orderDetails(savedOrder.getDishOrdered())
+                .orderId(savedOrder.getOrderId())
                 .build();
     }
 }
