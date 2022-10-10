01765d5d7a1d340db4fa87ee3a5d725c224a1eba
amitdatta <the.amisoft@gmail.com>
2022-10-10 11:33:40 +0530

    Service registry integrated

--- src/main/java/com/res/morderservice/MorderserviceApplication.java
+++ src/main/java/com/res/morderservice/MorderserviceApplication.java
@@ -2,8 +2,10 @@ package com.res.morderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MorderserviceApplication {
 
     public static void main(String[] args) {
